855b3de87af29bfec0198edd43f5a1870f21147c
Yana485 <105270905+Yana485@users.noreply.github.com>
2025-04-20 09:48:45 +0100

    task 4 - 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,39 @@
 /*відправлення форми form.login-form повинна відбуватися за подією submit.
 Під час відправлення форми сторінка не повинна перезавантажуватися.
+
 Якщо при сабміті у формі є незаповнені поля, виводь alert з попередженням про те,
- що 'All form fields must be filled in'. Не додавай на інпути атрибут required,
-  валідація має відбуватися саме через JS.
+що 'All form fields must be filled in'. Не додавай на інпути атрибут required,
+валідація має відбуватися саме через JS.
+
 Якщо користувач заповнив усі поля і відправив форму, збери значення полів 
 в об'єкт з двома властивостями, де ключ — це ім'я інпутів, а значення — 
 відповідні значення цих інпутів, очищені від пробілів по краях. 
+
 Для доступу до елементів форми використовуй властивість elements.
 При сабміті форми виведи об'єкт із введеними даними в консоль і 
 очисти значення полів форми методом reset. */
+
+const form = document.querySelector('form.login-form');
+
+const alert = document.createElement("p");
+form.insertAdjacentElement("afterend", alert);
+
+form.addEventListener("submit", sendForm);
+
+function sendForm(event) {
+  event.preventDefault();
+  const login = event.target;
+  const username = login.elements.email.value;
+  const password = login.elements.password.value;
+  const block = { Email: username.trim(), Password: password.trim() };
+
+  if (username.trim() === "" || password.trim() === "") {
+    alert.textContent = "All form fields must be filled in";
+  }
+  else {
+    alert.textContent = "";
+    console.log(block);
+  }
+
+  form.reset();
+};
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,20 @@
+/* Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль 
+по кліку на button.change-color і задає це значення кольору текстовим вмістом
+ для span.color.*/
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const paint = document.querySelector("body");
+const changeColour = document.querySelector("button.change-color");
+const text = document.querySelector(".color");
+
+changeColour.addEventListener("click", run);
+
+function run(event) {
+  paint.style.backgroundColor = getRandomHexColor();
+  text.textContent = paint.style.backgroundColor;
 }
